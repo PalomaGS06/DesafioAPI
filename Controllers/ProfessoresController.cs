a19e21944f5c7c17858518e80ebdabb78b3d93bb
Paloma Souza <paloma.souza112@gmail.com>
2022-09-01 01:22:57 -0300

    Controller da tabela Professor, construído

--- Controllers/ProfessoresController.cs
+++ Controllers/ProfessoresController.cs
@@ -1,5 +1,8 @@
-﻿using APICursosGratuitos.Repositories;
+﻿using APICursosGratuitos.Models;
+using APICursosGratuitos.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Data.SqlClient;
 
 namespace APICursosGratuitos.Controllers
 {
@@ -8,6 +11,157 @@ namespace APICursosGratuitos.Controllers
     public class ProfessoresController : ControllerBase
     {
         private ProfessoresRepository repositorio = new ProfessoresRepository();
-       
+        // POST - Cadastrar
+        /// <summary>
+        /// Cadastra os professores na aplicação
+        /// </summary>
+        /// <param name="Professores">Dados dos professores</param>
+        /// <returns>Dados do prof cadastrados com sucesso</returns>
+
+        [HttpPost]
+        public IActionResult Cadastrar([FromForm] Professores Professores)
+        {
+            try
+            {
+                repositorio.Insert(Professores);
+                return Ok(Professores);
+
+            }
+            catch (System.Exception e)
+            {
+                return StatusCode(500, new      //retorna o código e mensagem
+                {
+                    msg = "Falha na conexão",
+                    erro = e.Message,
+                });
+            }
+        }
+
+
+
+        // GET - Listar
+        /// <summary>
+        /// Lista todos os professores existentes
+        /// </summary>
+        /// <returns>Lista de Professores</returns>
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+
+                var prof = repositorio.GetAll();
+                return Ok(prof); //retorna a lista de professores
+
+            }
+            catch (System.Exception e)
+            {
+
+                return StatusCode(500, new
+                {
+                    msg = "Falha na conexão",
+                    erro = e.Message,
+                });
+            }
+
+        }
+
+
+        // PUT - Alterar
+        /// <summary>
+        /// Altera o email de um professor
+        /// </summary>
+        /// <param name="Cpf"> CPF do Professor</param>
+        /// <param name="Professor">Todas as informações do Professor</param>
+        /// <returns>Email Alterado!</returns>
+
+        [HttpPut("{CPF}")]
+        public IActionResult Alterar(int Cpf, Professores Professor)
+        {
+            try
+            {
+                var buscarProf = repositorio.GetById(Cpf);
+                if (buscarProf is null)
+                {
+                    return NotFound(new
+                    {
+                        msg = "CPF inválido!"
+                    });
+                }
+
+                var profEditado = repositorio.Update(Cpf, Professor);
+                return Ok(Professor);
+
+            }
+            catch (InvalidOperationException e)
+            {
+                return StatusCode(500, new
+                {
+                    msg = "Falha na conexão...",
+                    erro = e.Message,
+                });
+            }
+            catch (SqlException e)
+            {
+                return StatusCode(500, new
+                {
+                    msg = "Falha na sintaxe do código SQL...",
+                    erro = e.Message,
+                });
+            }
+            catch (System.Exception e)
+            {
+
+                return StatusCode(500, new
+                {
+                    msg = "Falha na definição do código",
+                    erro = e.Message,
+                });
+            }
+        }
+
+
+
+        // DELETE - Excluir
+        /// <summary>
+        /// Exclui um professor da aplicação
+        /// </summary>
+        /// <param name="Cpf">CPF do professor</param>
+        /// <returns>Mensagem de exclusão</returns>
+
+        [HttpDelete("{CPF}")]
+
+        public IActionResult Deletar(int Cpf)
+        {
+            try
+            {
+
+                var buscarProf = repositorio.GetById(Cpf);
+                if (buscarProf is null)
+                {
+                    return NotFound(new
+                    {
+                        msg = "CPF inválido!"
+                    });
+                }
+
+                repositorio.Delete(Cpf);
+
+                return Ok(new
+                {
+                    msg = "Professor excluído com sucesso!"
+                });
+            }
+            catch (System.Exception e)
+            {
+
+                return StatusCode(500, new
+                {
+                    msg = "Falha na conexão",
+                    erro = e.Message,
+                });
+            }
+
+        }
     }
 }
--- Repositories/ProfessoresRepository.cs
+++ Repositories/ProfessoresRepository.cs
@@ -1,6 +1,35 @@
-﻿namespace APICursosGratuitos.Repositories
+﻿using APICursosGratuitos.Interfaces;
+using APICursosGratuitos.Models;
+using System.Collections.Generic;
+
+namespace APICursosGratuitos.Repositories
 {
-    public class ProfessoresRepository : IProfessorRepository
+    public class ProfessoresRepository : IProfessoresRepository
     {
+        public Professores Delete(int Cpf)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICollection<Professores> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Professores GetById(int Cpf)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Professores Insert(Professores profs)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Professores Update(int Cpf, Professores profs)
+        {
+            throw new System.NotImplementedException();
+        }
     }
+
 }
