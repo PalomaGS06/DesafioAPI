ac04785c9a03ecd6cb41f0fda7b6385f6313d146
Paloma Souza <paloma.souza112@gmail.com>
2022-09-01 02:15:36 -0300

    Construído o Repository da tabela Professores

--- Repositories/ProfessoresRepository.cs
+++ Repositories/ProfessoresRepository.cs
@@ -1,32 +1,177 @@
 ﻿using APICursosGratuitos.Interfaces;
 using APICursosGratuitos.Models;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace APICursosGratuitos.Repositories
 {
     public class ProfessoresRepository : IProfessoresRepository
     {
-        public Professores Delete(int Cpf)
-        {
-            throw new System.NotImplementedException();
+        // Cria uma string de conexão com o Banco de Dados
+        //variável de apenas leitura = readonly
+
+        readonly string connectionString = "Data Source=WORKSTATIONSOUZ\\SQLEXPRESS;Integrated Security=true;Initial Catalog=CursosGratuitos";
+
+
+        //Deletar o professor através do CPF - DELETE
+        public bool Delete(int Cpf)
+        {          
+                using (SqlConnection conexao = new SqlConnection(connectionString)) // dentro do parametro se passa a string de conexao
+                {
+                    conexao.Open(); // abrir conexão
+
+                    // escreve a consulta de exclusão
+                    string script = "DELETE FROM Professores WHERE CPF=@Cpf";
+
+                    // Comando de execução no banco criado
+                    using (SqlCommand cmd = new SqlCommand(script, conexao))
+                    {
+                        //Declarações das variaveis por parametros
+                        cmd.Parameters.Add("@Cpf", SqlDbType.Int).Value = Cpf;
+
+                        // Tipo de comando, tipo texto. CommandType é um Enum
+                        cmd.CommandType = CommandType.Text;
+                        int linhasAfetadas = cmd.ExecuteNonQuery(); //Retorna os numeros (CPF) das linhas alteradas/afetadas 
+                        if (linhasAfetadas == 0)
+                        {
+                            return false; // não retorna nada
+                        }
+                    }
+
+                }
+
+              return true;
+            
+
         }
 
+        //Buscar tudo - GET
         public ICollection<Professores> GetAll()
         {
-            throw new System.NotImplementedException();
+            var professores = new List<Professores>(); // Criando um objeto do tipo lista para exibir todas as colunas e dados da tabela Professores
+
+            using (SqlConnection conexao = new SqlConnection(connectionString))
+            {
+                conexao.Open();
+
+                // Escreve a consulta/script de busca
+                string consulta = "SELECT * FROM Professores";
+
+                using (SqlCommand cmd = new SqlCommand(consulta, conexao))
+                {
+                    // Ler todos os itens da consulta
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        // Colunas lidas através de um laço condicional
+                        while (reader.Read())
+                        {
+                            professores.Add(new Professores
+                            {
+                                // Cada coluna, criada pelo Model, representa uma coluna da tabela Professores, contendo seus respectivos tipo de dado e posição de índice da array
+                                Cpf = (int)reader[0],
+                                Nome = (string)reader[1],
+                                Email = (string)reader[2]
+                            });
+                        }
+                    }
+                }
+
+            }
+
+            return professores; // retorna a lista professores
         }
 
+        //Buscar por CPF - GET
         public Professores GetById(int Cpf)
         {
-            throw new System.NotImplementedException();
+            Professores profs = null; // Objeto criado atraves da classe Professores
+
+            using (SqlConnection conexao = new SqlConnection(connectionString))
+            {
+                conexao.Open(); // abre a conexão
+
+                // Escreve a script de busca por CPF
+                string consulta = "SELECT * FROM Professores WHERE CPF=@Cpf";
+
+                using (SqlCommand cmd = new SqlCommand(consulta, conexao))
+                {
+
+                    cmd.Parameters.Add("@Cpf", SqlDbType.Int).Value = Cpf;
+
+                    // Ler todos os itens da consulta
+                    using (SqlDataReader result = cmd.ExecuteReader())
+                    {
+                        while (result.Read())
+                        {
+                            profs = new Professores
+                            {
+                                Cpf = (int)result[0],                 
+                                Nome = (string)result[1],                               
+                                Email = (string)result[2]
+                               
+                            };
+
+                        }
+                    }
+                }
+
+            }
+            return profs; // retorna o professor do CPF selecionado
         }
 
+        //Cadastra os dados do professor - INSERT
         public Professores Insert(Professores profs)
         {
-            throw new System.NotImplementedException();
+            using (SqlConnection conexao = new SqlConnection(connectionString)) // dentro do parametro se passa a string de conexao
+            {
+                conexao.Open();   // Abre uma conexao
+
+                // escrever a consulta de inserção
+                string script = "INSERT INTO Professores (Nome, Email) VALUES (@Nome, @Email)";
+
+                // Criamos o comando de execução no banco
+                using (SqlCommand cmd = new SqlCommand(script, conexao))
+                {
+                    //fazemos as declarações das variaveis por parametros
+                    cmd.Parameters.Add("@Nome", SqlDbType.NVarChar).Value = profs.Nome;
+                    cmd.Parameters.Add("@Email", SqlDbType.NVarChar).Value = profs.Email;
+
+                    cmd.CommandType = CommandType.Text; // Tipo de comando Enum, do tipo texto.
+                    cmd.ExecuteNonQuery(); //returna o número de linhas afetadas
+                }
+
+            }
+
+            return profs;
         }
 
+        //Altera algum dado do professor - UPDATE
         public Professores Update(int Cpf, Professores profs)
+        {
+            using (SqlConnection conexao = new SqlConnection(connectionString)) // dentro do parametro se passa a string de conexao
+            {
+                conexao.Open(); //conexão iniciada
+
+                // escrever a consulta de atualização do email
+                string script = "UPDATE Professores SET Email=@Email WHERE CPF=@Cpf";
+
+                // Criamos o comando de execução no banco
+                using (SqlCommand cmd = new SqlCommand(script, conexao))
+                {
+                    //Declarações da variável Email por parametro
+                    cmd.Parameters.Add("@Email", SqlDbType.NVarChar).Value = profs.Email;
+
+                    // Tipo de comando, tipo texto
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            return profs; //atributo alterado, do parâmetro profs 
+        }
+
+        Professores IProfessoresRepository.Delete(int Cpf)
         {
             throw new System.NotImplementedException();
         }
