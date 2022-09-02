7b68bb1e2b26c59f6c308c305d4699485ac4e711
Paloma Souza <paloma.souza112@gmail.com>
2022-09-02 03:11:14 -0300

    Interface da tabela Aulas criada

--- Interfaces/IAulasRepository.cs
+++ Interfaces/IAulasRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace APICursosGratuitos.Interfaces
+﻿using APICursosGratuitos.Models;
+using System.Collections.Generic;
+
+namespace APICursosGratuitos.Interfaces
 {
     public interface IAulasRepository
     {
+
+        ICollection<Aulas> GetAll();
+
+        //SELECT
+        Aulas GetById(int id);
+
+        //CREATE
+        Aulas Insert(Aulas aulas);
+
+        //UPDATE
+        Aulas Update(int id, Aulas aulas);
+
+        //DELETE
+        Aulas Delete(int id);
     }
 }
