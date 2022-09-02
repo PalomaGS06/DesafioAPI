47a45c4e8b5abd74e4a8388e9392458c5fc77b60
Paloma Souza <paloma.souza112@gmail.com>
2022-09-02 03:09:15 -0300

    Model da tabela Aulas atualizado

--- Models/Aulas.cs
+++ Models/Aulas.cs
@@ -1,6 +1,17 @@
-﻿namespace APICursosGratuitos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICursosGratuitos.Models
 {
     public class Aulas
     {
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Informar o título da aula!")]
+        public string Titulo { get; set; }
+        public string Ementa { get; set; }
+        public int Duracao { get; set; }                
+        public Cursos Curso { get; set; }               
+        public Professores Professor { get; set; }
     }
 }
