5b865a83d68b62d91fd6460e833a5b9126ff7417
Paloma Souza <paloma.souza112@gmail.com>
2022-09-02 01:36:45 -0300

    Atualizada a Interface da tabela Cursos

--- Interfaces/ICursosRepository.cs
+++ Interfaces/ICursosRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace APICursosGratuitos.Interfaces
+﻿using APICursosGratuitos.Models;
+using System.Collections.Generic;
+
+namespace APICursosGratuitos.Interfaces
 {
     public interface ICursosRepository
     {
+        ICollection<Cursos> GetAll();
+
+        //SELECT
+        Cursos GetById(int id);
+
+        //CREATE
+        Cursos Insert(Cursos cursos);
+
+        //UPDATE
+        Cursos Update(int id, Cursos cursos);
+
+        //DELETE
+        Cursos Delete(int id);
     }
 }
