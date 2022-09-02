70ff35fccf697c9fe7f90b502a3920b6fde2db94
Paloma Souza <paloma.souza112@gmail.com>
2022-09-02 02:39:10 -0300

    Inteface da tabela AlunoCurso criada

--- Interfaces/IAlunoCursoRepository.cs
+++ Interfaces/IAlunoCursoRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace APICursosGratuitos.Interfaces
+﻿using APICursosGratuitos.Models;
+using System.Collections.Generic;
+
+namespace APICursosGratuitos.Interfaces
 {
     public interface IAlunoCursoRepository
     {
+        ICollection<AlunoCurso> GetAll();
+
+        //SELECT
+        AlunoCurso GetById(int id);
+
+        //CREATE
+        AlunoCurso Insert(AlunoCurso alunoCurso);
+
+        //UPDATE
+        AlunoCurso Update(int id, AlunoCurso alunoCurso);
+
+        //DELETE
+        AlunoCurso Delete(int id);
     }
 }
