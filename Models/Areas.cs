ec7f926da5242ad615fe557ff8114671689ce38e
Paloma Souza <paloma.souza112@gmail.com>
2022-09-02 01:31:42 -0300

    Model atualizado da tabela Cursos e JsonIgnoreCondition adicionado no Model da tabela Areas

--- Models/Areas.cs
+++ Models/Areas.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace APICursosGratuitos.Models
 {
@@ -6,9 +7,11 @@ namespace APICursosGratuitos.Models
     {
         public int Id { get; set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         [Required(ErrorMessage = "Favor, informe uma area!")]
-        public string Area { get; set; }    
+        public string Area { get; set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public string Imagem { get; set; }
     }
 }
--- Models/Cursos.cs
+++ Models/Cursos.cs
@@ -1,6 +1,19 @@
-﻿namespace APICursosGratuitos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICursosGratuitos.Models
 {
     public class Cursos
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informar o nome do curso!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informar as horas de curso!")]
+        public int CargaHoraria { get; set; }
+
+        [Required(ErrorMessage = "Informar a area do curso!")]
+        public Areas Area { get; set; }
+        public string Imagem { get; set; }
     }
 }
